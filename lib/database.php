757dbde109eb0cca84a1cd6613f2db5535f27925
David Monaghan <github@allstruck.com>
2012-12-02 19:49:58 -0700

    First cleaning run on db code.

--- lib/database.php
+++ lib/database.php
@@ -1,11 +1,22 @@
 <?php
 
+global $wpdb, $table_prefix;
+$crowdioVoteTableName = $table_prefix . "_crowdio_vote";
+$crowdioCommentTableName = $table_prefix . "_crowdio_comment";
+
 $ranking = "
-			SELECT comment_id, ((positive + 1.9208) / (pos + negative) - 1.96 * SQRT((positive * negative) / (positive + negative) + 0.9604) / 
-			   (positive + negative)) / (1 + 3.8416 / (positive + negative)) 
-			   AS ci_lower_bound FROM "$crowdio_vote" WHERE positive + negative > 0 
-			   ORDER BY ci_lower_bound DESC;
-       ";
-
-
-?>
+			SELECT comment_id, 
+				(
+					(positive + 1.9208) / 
+					(pos + negative) - 
+					1.96 * SQRT(
+						(positive * negative) / 
+						(positive + negative) + 0.9604) / 
+			   			(positive + negative)
+			   	) / t
+				(1 + 3.8416 / (positive + negative)) 
+			   	AS ci_lower_bound 
+			   	FROM '$crowdioVoteTableName' 
+			   	WHERE positive + negative > 0 
+			   	ORDER BY ci_lower_bound DESC;
+			";
