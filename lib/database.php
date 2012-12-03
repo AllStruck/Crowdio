e67b02789cfe2add62fbf0c48ab34d9d90a94f43
David Monaghan <github@allstruck.com>
2012-12-03 13:06:27 -0700

    Created class for database operations.

--- lib/database.php
+++ lib/database.php
@@ -1,49 +1,53 @@
 <?php
-
-global $wpdb, $table_prefix;
-$crowdioVoteTableName = $table_prefix . "_crowdio_vote";
-$crowdioCommentTableName = $table_prefix . "_crowdio_comment";
-
-
-
-
-function vote_rank($type)
+/**
+*/
+class CrowdioDatabase extends Crowdio
 {
-	switch ($type) {
-		case 'question':
-			$comment_id = "";
-			$table = "";
-
-			break;
-		
-		case 'comment':
-			$comment_id = "";
-			$table = "";
-					break;
-
-		case 'reply':
-			$comment_id = "";
-			$table = "";
-			break;
+	
+	function __construct()
+	{
+		global $wpdb, $table_prefix;
+		$crowdio_vote_table_name = $table_prefix . "_crowdio_vote";
+		$crowdio_comment_table_name = $table_prefix . "_crowdio_comment";
 	}
 
-	$ranking = "
-			SELECT '$comment_id', 
-				(
-					(positive + 1.9208) / 
-					(pos + negative) - 
-					1.96 * SQRT(
-						(positive * negative) / 
-						(positive + negative) + 0.9604) / 
-			   			(positive + negative)
-			   	) /
-				(1 + 3.8416 / (positive + negative)) 
-			   	AS ci_lower_bound 
-			   	FROM '$table' 
-			   	WHERE positive + negative > 0 
-			   	ORDER BY ci_lower_bound DESC;
-			";
+	function get_ranked_votes($type)
+	{
+		switch ($type) {
+			case 'question':
+				$comment_id = "";
+				$table = "";
+				break;
+			
+			case 'comment':
+				$comment_id = "";
+				$table = "";
+				break;
 
-	# need db connect for ranking
+			case 'reply':
+				$comment_id = "";
+				$table = "";
+				break;
+		}
 
+		$ranking_query = "
+				SELECT '$comment_id', 
+					(
+						(positive + 1.9208) / 
+						(pos + negative) - 
+						1.96 * SQRT(
+							(positive * negative) / 
+							(positive + negative) + 0.9604) / 
+				   			(positive + negative)
+					) /
+					(1 + 3.8416 / (positive + negative)) 
+					AS ci_lower_bound 
+					FROM '$table' 
+					WHERE positive + negative > 0 
+					ORDER BY ci_lower_bound DESC;";
+
+		return $wpdb->get_result($ranking_query);
+	}
 }
+
+
