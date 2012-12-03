bac34311dffe73e512457496b663bfd8f3bfd4c0
Dommert <DommertEnterprises@Gmail.com>
2012-12-02 22:54:20 -0700

    rank function

--- lib/database.php
+++ lib/database.php
@@ -4,8 +4,31 @@ global $wpdb, $table_prefix;
 $crowdioVoteTableName = $table_prefix . "_crowdio_vote";
 $crowdioCommentTableName = $table_prefix . "_crowdio_comment";
 
-$ranking = "
-			SELECT comment_id, 
+
+
+
+function vote_rank($type)
+{
+	switch ($type) {
+		case 'question':
+			$comment_id = "";
+			$table = "";
+
+			break;
+		
+		case 'comment':
+			$comment_id = "";
+			$table = "";
+					break;
+
+		case 'reply':
+			$comment_id = "";
+			$table = "";
+			break;
+	}
+
+	$ranking = "
+			SELECT '$comment_id', 
 				(
 					(positive + 1.9208) / 
 					(pos + negative) - 
@@ -13,10 +36,14 @@ $ranking = "
 						(positive * negative) / 
 						(positive + negative) + 0.9604) / 
 			   			(positive + negative)
-			   	) / t
+			   	) /
 				(1 + 3.8416 / (positive + negative)) 
 			   	AS ci_lower_bound 
-			   	FROM '$crowdioVoteTableName' 
+			   	FROM '$table' 
 			   	WHERE positive + negative > 0 
 			   	ORDER BY ci_lower_bound DESC;
 			";
+
+	# need db connect for ranking
+
+}
