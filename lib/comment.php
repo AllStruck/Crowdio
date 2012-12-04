1b3088c4516da8994c0150eb76d0137faf8adb45
Dommert <DommertEnterprises@Gmail.com>
2012-12-03 22:43:21 -0700

    updates2

--- lib/comment.php
+++ lib/comment.php
@@ -13,10 +13,20 @@ class ClassName extends AnotherClass
 	function draw_comment_form() {
 		$comment_id;
 		print <<<END
-			<form>
-				<input />
-				<input type="submit" />
-			</form>
+		
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+Name: <input type="text" name="name" id="<?php $_POST['name']; ?>"> <BR>
+Email: <input type="text" name="email" id="<?php $_POST['email']; ?>"> <BR>
+Company: <input type="text" name="company" id="<?php $_POST['company']; ?>"> <BR>
+Comment: <textarea name="comment" id="<?php $_POST['comment']; ?>"></textarea>
+</form>
+
 END
 	}
 }
+
+
+
+
+
+
