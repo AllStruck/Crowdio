b1074b33f6f8c5dc4d5426b6ce37abc0b4de3c5a
David Monaghan <github@allstruck.com>
2012-12-03 18:42:27 -0700

    Filling out setup.php

--- lib/setup.php
+++ lib/setup.php
@@ -1,3 +1,8 @@
 <?php
+// Globals and constants.
+global $wpdb, $table_prefix;
 
 define('CROWDIO_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('CROWDIO_COMMENT_TABLE_NAME', $table_prefix . 'crowdio_comments');
+define('CROWDIO_VOTE_TABLE_NAME', $table_prefix . 'crowdio_votes');
+
