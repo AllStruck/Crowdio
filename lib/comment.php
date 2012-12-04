605a14404ed24a1c5c0b266612784fe3cc234659
David Monaghan <github@allstruck.com>
2012-12-03 21:05:12 -0700

    Reorganizing and cleaning up.

---
+++ lib/comment.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+*/
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+
+	function draw_comment_form() {
+		$comment_id;
+		print <<<END
+			<form>
+				<input />
+				<input type="submit" />
+			</form>
+END
+	}
+}
