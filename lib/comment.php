a7b634a770333e62f55e0ab7aa0f8dee3dec2532
David Monaghan <github@allstruck.com>
2012-12-09 18:57:54 -0700

    Added missing indent level in display_comment_form()

--- lib/comment.php
+++ lib/comment.php
@@ -13,33 +13,32 @@ class CrowdioComment extends Crowdio {
 
 	function display_comment_form()
 	{
-	$action_url = $GLOBALS['post']->guid;
-	$user_name = $_POST['name'];
-	$user_email = $_POST['email'];
-	$user_company = $_POST['company'];
-	$user_website = $_POST['user_website'];
-	$user_comment = $_POST['comment'];
-	
-	print <<<END
-		<section class="crowdio_form">
-		    <form method="post" action="$action_url">
-		    	<input type="hidden" name="crowdio_id" value="$rfi_id" />
-		    	
-			    <div class="crowdio_row"> <span id="crowdio_comment_form">COMMENTS</span> </div>
-			    
-			    <fieldset>
-			    	<legend>Personalia:</legend>
-					<label for="crowdio_name">Name:</label> <input type="text" id="crowdio_name" />
-	    		</fieldset>
-	
-		        <div class="crowdio_row"> <field>Name: </field> <input type="text" name="name" id="$user_name"> </div>
-		        <div class="crowdio_row"> <field>Email: </field><input type="email" name="email" id="$user_email"> </div>
-		        <div class="crowdio_row"> <field>Company: </field> <input type="text" name="company" id="$user_company"> </div>
-		        <div class="crowdio_row"> <field>Website: </field> <input type="text" name="website" id="$user_website"> </div>
-		        <div class="crowdio_row"> <field>Comment: </field><textarea name="comment" id="$user_comment"></textarea> </div>
-		        <div class="crowdio_row"> <field>&nbsp; </field> <input type="submit" value="SUBMIT!!" id="submit"></div>
-		    </form>
-		</section>
+		$action_url = $GLOBALS['post']->guid;
+		$user_name = $_POST['name'];
+		$user_email = $_POST['email'];
+		$user_company = $_POST['company'];
+		$user_website = $_POST['user_website'];
+		$user_comment = $_POST['comment'];
+		
+		print <<<END
+			<section class="crowdio_form">
+			    <form method="post" action="$action_url">
+			    	<input type="hidden" name="crowdio_id" value="$rfi_id" />
+			    	
+				    <div class="crowdio_row"> <span id="crowdio_comment_form">COMMENTS</span> </div>
+				    
+				    <fieldset>
+						<label for="crowdio_name">Name:</label> <input type="text" id="crowdio_name" />
+		    		</fieldset>
+		
+			        <div class="crowdio_row"> <field>Name: </field> <input type="text" name="name" id="$user_name"> </div>
+			        <div class="crowdio_row"> <field>Email: </field><input type="email" name="email" id="$user_email"> </div>
+			        <div class="crowdio_row"> <field>Company: </field> <input type="text" name="company" id="$user_company"> </div>
+			        <div class="crowdio_row"> <field>Website: </field> <input type="text" name="website" id="$user_website"> </div>
+			        <div class="crowdio_row"> <field>Comment: </field><textarea name="comment" id="$user_comment"></textarea> </div>
+			        <div class="crowdio_row"> <field>&nbsp; </field> <input type="submit" value="Submit" id="submit"></div>
+			    </form>
+			</section>
 END;
 	}
 
